81cc813da6215cd84f9a4a344e8729e6d5dd2509
QuangVo <quanghuyvo90@gmail.com>
2023-12-08 16:23:40 -0800

    Passport JWT

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const session = require("express-session");
-const FileStore = require("session-file-store")(session);
 const passport = require('passport');
-const authenticate = require('./authenticate');
+const config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,27 +14,24 @@ const partnerRouter = require('./routes/partnerRouter');
 require('dotenv').config();
 
 const mongoose = require('mongoose');
-// const url = 'mongodb://localhost:27017/nucampsite';
-// const connect = mongoose.connect(url, {
+const url = config.mongoUrl;
+const connect = mongoose.connect(url, {
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// MongoDB Config
+// const mongodbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/nucampsite';
+
+// const connect = mongoose.connect(mongodbURI, {
 //   useCreateIndex: true,
 //   useFindAndModify: false,
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true
 // });
 
-// connect.then(() => console.log('Connected correctly to server'),
-//   err => console.log(err)
-// );
-
-// MongoDB Config
-const mongodbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/nucampsite';
-
-const connect = mongoose.connect(mongodbURI, {
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
 connect.then(() => console.log('Connected correctly to server'),
   err => console.log(err)
 );
@@ -53,32 +47,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser('12345-67890-09876-54321'));
 
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
-
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-function auth(req, res, next) {
-  console.log(req.user);
-  if (!req.user) {
-    const err = new Error("You are not authenticated!");
-    err.status = 401;
-    return next(err);
-  } else {
-    return next();
-  }
-}
-
-app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/campsites', campsiteRouter);
